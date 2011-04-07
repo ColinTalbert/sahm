a4578e6a95d4c4bdc23c1ae148030d29e37cb611
Colin Talbert <talbertc@usgs.gov>
2011-04-07 10:15:45 -0600

    Layers.csv is now found relative to the __file__ path
    Signed-off-by: Colin Talbert <talbertc@usgs.gov>

--- __init__.py
+++ __init__.py
@@ -8,7 +8,6 @@ sahm_path = None
 models_path = None
 configuration = \
     ConfigurationObject(output_dir= r'I:\VisTrails\WorkingFiles\workspace',
-                        layers_path = r'I:\VisTrails\Central_VisTrailsInstall\VisTrails\vistrails\packages\sahm\layers.csv',
                         r_path = r'I:\VisTrails\Central_VisTrailsInstall\Central_R\R-2.12.1\bin',
                         gdal_path = r'I:\VisTrails\Central_VisTrailsInstall\Central_GDAL',
                         verbose = 'True')
