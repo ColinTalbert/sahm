305dcbaa64a758eef27a9652ad6b440728e80d0d
Colin Talbert <talbertc@usgs.gov>
2011-08-25 17:34:25 -0600

    Part of the changes required to make the whole package more distributable.

--- __init__.py
+++ __init__.py
@@ -6,9 +6,9 @@ version = '0.0.5'
 
 
 configuration = \
-    ConfigurationObject(output_dir= r'I:\VisTrails\WorkingFiles\workspace',
-                        r_path = r'I:\VisTrails\Central_VisTrailsInstall\Central_R\R-2.12.1\bin',
-                        gdal_path = r'I:\VisTrails\Central_VisTrailsInstall\Central_GDAL',
-                        maxent_path = r'I:\VisTrails\Central_VisTrailsInstall\Central_Maxent',
-                        projection_layers_path = r'I:\WorldClim_Future_Climate\RenamedBILs',
+    ConfigurationObject(output_dir= r'C:\temp\SAHM_workspace',
+                        r_path = r'..\\Central_R\R-2.12.1\bin',
+                        gdal_path = r'..\\Central_GDAL',
+                        maxent_path = r'..\\Central_Maxent',
+                        projection_layers_path = r'I:\WorldClim_Future_Climate\RenamedBILs', #This only applies to instances running in the FORT Infrastructure
                         verbose = 'True')
