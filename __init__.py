f3715347760142241362f657e7ccf864058c2d15
Colin Talbert <talbertc@usgs.gov>
2011-12-28 14:36:39 -0700

    Change to the relative location we look for dependencies from.

--- __init__.py
+++ __init__.py
@@ -6,8 +6,8 @@ version = '1.0.0.rc1'
 
 configuration = \
     ConfigurationObject(output_dir= r'C:\temp\SAHM_workspace',
-                        r_path = r'..\\Central_R\R-2.12.1\bin',
-                        gdal_path = r'..\\Central_GDAL',
-                        maxent_path = r'..\\Central_Maxent',
+                        r_path = r'..\\..\\Central_R\R-2.14.1\bin',
+                        gdal_path = r'..\\..\\Central_GDAL',
+                        maxent_path = r'..\\..\\Central_Maxent',
                         projection_layers_path = r'I:\WorldClim_Future_Climate\RenamedBILs', #This only applies to instances running in the FORT Infrastructure
                         verbose = 'True')
