88585a743e97de8f6163a9959cb3d253a57422f4
unknown <mtalbert@.gs.doi.net>
2012-01-05 11:57:38 -0700

    response curve changes

--- pySAHM/Resources/R_Modules/response.curves.r
+++ pySAHM/Resources/R_Modules/response.curves.r
@@ -1,7 +1,14 @@
-response.curves<-function(out,Model,bname){
- attach(out$input)
- on.exit(detach(out$input))
- bname<-out$dat$bname
+response.curves<-function(out,Model,pred.dat=NULL,cv=FALSE){
+
+ if(cv==TRUE){ #avoid producing pdfs here we just want the values for later
+  debug.mode=FALSE
+  responseCurveForm="no"
+  }  else{
+     attach(out$input)
+      on.exit(detach(out$input))
+      bname<-out$dat$bname
+  }
+
  if(Model=="mars"){
         if(is.null(responseCurveForm)){
            responseCurveForm<-0}
@@ -15,7 +22,7 @@ response.curves<-function(out,Model,bname){
 
              } else r.curves<-mars.plot(out$mods$final.mod,plot.it=F)
      }
-     
+
  if(Model=="glm") {if(debug.mode | responseCurveForm=="pdf"){
         nvar <- length(coef(out$mods$final.mod))-1
         pcol <- min(ceiling(sqrt(nvar)),4)
@@ -25,9 +32,26 @@ response.curves<-function(out,Model,bname){
             par(oma=c(2,2,4,2),mfrow=c(prow,pcol))
             r.curves <-my.termplot(out$mods$final.mod,plot.it=T)
             mtext(paste("GLM response curves for",basename(ma.name)),outer=T,side=3,cex=1.3)
+           ################### New work
+           #starting by just looking at one element of the list later use an lapply
+            if(!is.null(out$cv$resp.curves))
+                    for(i in 1:length(r.curves$names)){
+                          plot(r.curves$pred[[i]],r.curves$resp[[i]],type="l")
+                          for(j in 1:length(out$cv$resp)){
+                           indx<-na.omit(match(r.curves$names[i],names(out$cv$resp[[i]]$pred)))
+                           lines(out$cv$resp.curves[[j]]$pred[indx][[1]],out$cv$resp.curves[[j]]$resp[indx][[1]],col="grey",lty="dotdash")
+                           }
+                         lines(r.curves$pred[[i]],r.curves$resp[[i]],type="l",col="red",lwd=2)
+                           }
+
+
+                      plot(r.curves$pred[1][[1]], r.curves$resp[1][[1]])
+                          points(out$cv$resp.curves$"1"$pred[1][[1]][1],out$cv$resp.curves$"1"$resp[1][[1]][1])
+
+           #############################
             par(mfrow=c(1,1))
             graphics.off()
-        } else out$mods$r.curves<-my.termplot(out$mods$final.mod,plot.it=F)
+        } else r.curves<-my.termplot(out$mods$final.mod,plot.it=F)
 
      }
 if(Model=="rf"){
@@ -55,7 +79,7 @@ if(Model=="rf"){
                         cat(paste("Progress:",round(70+i*inc,1),"%\n",sep=""));flush.console()  ### print time
                         }
                 if(debug.mode) graphics.off()
-                out$mods$r.curves <- r.curves
+
 
     }
 if(Model=="brt"){
@@ -80,7 +104,10 @@ if(Model=="brt"){
                     }
             graphics.off()
             } else {
-                out$mods$r.curves <- gbm.plot(out$mods$final.mod,plotit=F)
+                r.curves <- gbm.plot(out$mods$final.mod,plotit=F)
             }
     }
+    if(cv==FALSE) r.curves<-NULL
+    return(r.curves)
+    
 }
