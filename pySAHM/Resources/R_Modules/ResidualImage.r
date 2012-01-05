9a5fd1bdea841d960f706589632c7a845bf3230c
unknown <mtalbert@.gs.doi.net>
2012-01-05 10:35:56 -0700

    fixing a small issue with the residual image scale not being related to anything

--- pySAHM/Resources/R_Modules/ResidualImage.r
+++ pySAHM/Resources/R_Modules/ResidualImage.r
@@ -1,10 +1,14 @@
 resid.image<-function(dev.contrib,pred,raw.dat,x,y,model.type,file.name,out,label){
-          browser()
+
           #for some reason dev.contrib is negative for binomial and bernoulli and positive for poisson
    if(label!="eval") z<-sign(pred-raw.dat)*abs(dev.contrib)
       else z<-pred-raw.dat
      MinCol<-min(z)
      MaxCol<-max(z)
+         col.i<- beachcolours(heightrange=c(min(z),max(z)),sealevel=0,s=1,ncolours=(length(table(z))+1))
+         f<-function(a,b) sqrt((a-b)^2)
+           s1<-seq(from=MinCol,to=MaxCol,length=length(table(z)))
+         col.ind<-apply((outer(s1,z,f)),2,which.min)
 
               #####################################################
               a<-loess(z~x*y)
@@ -20,9 +24,10 @@ resid.image<-function(dev.contrib,pred,raw.dat,x,y,model.type,file.name,out,labe
               jpeg(file=paste(out$dat$bname,"resid.plot.jpg",sep="."),width=1000,height=1000,pointsize=13)
                  par(oma=c(3,3,3,3))
                  layout(matrix(data=c(1,2), nrow=1, ncol=2), widths=c(4,1), heights=c(1,1))
-                  image(z,x=x.lim,y=y.lim,col=beachcolours(heightrange=c(min(z),max(z)),sealevel=0,ncolours=length(table(z))),
+                  image(z,x=x.lim,y=y.lim,col=beachcolours(heightrange=c(min(z),max(z)),sealevel=0,s=.5,ncolours=length(table(z))),
                   main=paste("Spatial pattern of", ifelse(label!="eval"," deviance residuals\n(magnitude and sign)"," prediction error")),xlab="X coordinate",ylab="Y coordinate")
-                  points(x,y,bg=c("blue","red")[sign(pred-raw.dat)/2+1.5], pch=21,cex=abs(dev.contrib)/max(abs(dev.contrib))*5)
+
+                  points(x,y,bg=col.i[col.ind], pch=21,cex=abs(dev.contrib)/max(abs(dev.contrib))*5)
                   par(mar = c(3,2.5,2.5,2))
 
              colrange<-seq(from=MinCol,to=MaxCol,length=100)
