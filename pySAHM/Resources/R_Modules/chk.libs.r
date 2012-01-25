91684a337f9f7c0ffd73b34ec56816c285e82769
unknown <mtalbert@.gs.doi.net>
2012-01-25 09:53:33 -0700

    update libraries

--- pySAHM/Resources/R_Modules/chk.libs.r
+++ pySAHM/Resources/R_Modules/chk.libs.r
@@ -1,9 +1,9 @@
 chk.libs <- function(Model){
 
-     if(Model=="mars") libs<-list("PresenceAbsence","rgdal","sp","survival","mda","raster","tcltk2","foreign","ade4")
-     if(Model=="glm")  libs<-list("PresenceAbsence","rgdal","sp","survival","tools","raster","tcltk2","foreign","ade4")
-     if(Model=="rf")   libs<-list("randomForest","PresenceAbsence","rgdal","sp","raster","tcltk2","foreign","ade4")
-     if(Model=="brt")  libs<-list("PresenceAbsence","rgdal","sp","survival","lattice","raster","tcltk2","foreign","ade4","gbm")
+     if(Model=="mars") libs<-list("PresenceAbsence","rgdal","sp","survival","mda","raster","tcltk2","foreign","ade4","ROCR")
+     if(Model=="glm")  libs<-list("PresenceAbsence","rgdal","sp","survival","tools","raster","tcltk2","foreign","ade4","ROCR")
+     if(Model=="rf")   libs<-list("randomForest","PresenceAbsence","rgdal","sp","raster","tcltk2","foreign","ade4","ROCR")
+     if(Model=="brt")  libs<-list("PresenceAbsence","rgdal","sp","survival","lattice","raster","tcltk2","foreign","ade4","gbm","ROCR")
      
       lib.mssg <- unlist(suppressMessages(suppressWarnings(lapply(libs,require,quietly = T, warn.conflicts=F,character.only=T))))
       if(any(!lib.mssg)){
