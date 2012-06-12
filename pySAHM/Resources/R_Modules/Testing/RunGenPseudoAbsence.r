0418b154c971697fb6ac6ea926f90de7ad40a73b
unknown <mtalbert@.gs.doi.net>
2012-06-12 15:23:49 -0600

    small bug fix

--- pySAHM/Resources/R_Modules/Testing/RunGenPseudoAbsence.r
+++ pySAHM/Resources/R_Modules/Testing/RunGenPseudoAbsence.r
@@ -1,11 +1,13 @@
+output.dir="I:\\VisTrails\\VisTrails_SAHM_x32_debug\\VisTrails\\vistrails\\packages\\TestingRCode\\FDAW_2_MCP.tif"
 
 input.file="J:\\Projects\\Climate_RS_Comparison\\Cheatgrass_VisTrails\\FDAW_1.csv"
-
+input.file="I:\\VisTrails\\VisTrails_SAHM_x32_debug\\VisTrails\\vistrails\\packages\\TestingRCode\\FDAW_2.csv"
 output.dir<-"J:\\Projects\\Climate_RS_Comparison\\Cheatgrass_VisTrails\\BackgroundPtSurfaces\\95AdHocIsoplethKDE.tif"
 PseudoAbsGen(input.file,output.dir,method="KDE",bw.otim="adhoc",isopleth=95,bias=FALSE)
+template<-"I:\\VisTrails\\VisTrails_SAHM_x32_debug\\VisTrails\\vistrails\\packages\\TestingRCode\\PARC_100mTemplate\\bio_03_1971_2000_800m.tif"
 
 output.dir<-"J:\\Projects\\Climate_RS_Comparison\\Cheatgrass_VisTrails\\BackgroundPtSurfaces\\95MCP.tif"
-PseudoAbsGen(input.file,output.dir,method="MCP",bw.otim="adhoc",isopleth=95,bias=FALSE)
+PseudoAbsGen(input.file,output.dir,method="MCP",bw.otim="adhoc",isopleth=95,bias=FALSE,template=template)
 
 output.dir<-"J:\\Projects\\Climate_RS_Comparison\\Cheatgrass_VisTrails\\BackgroundPtSurfaces\\AdHocContinKDE.tif"
 PseudoAbsGen(input.file,output.dir,method="KDE",bw.otim="adhoc",isopleth=95,bias=TRUE)
