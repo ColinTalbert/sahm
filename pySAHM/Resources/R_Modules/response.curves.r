1a99587679d1ced43c8d041d77c1afe3594cd452
unknown <mtalbert@.gs.doi.net>
2012-10-18 15:37:43 -0600

    standardizing response curves

--- pySAHM/Resources/R_Modules/response.curves.r
+++ pySAHM/Resources/R_Modules/response.curves.r
@@ -47,14 +47,12 @@ response.curves<-function(out,Model,pred.dat=NULL,cv=FALSE){
       on.exit(detach(out$input))
       bname<-out$dat$bname
   
-      if(Model%in%c("brt","mars"))  nvar <- nrow(out$mods$summary)
+      if(Model%in%c("brt","mars","rf"))  nvar <- nrow(out$mods$summary)
       if(Model=="glm")              nvar <- out$mods$n.vars.final-length(grep(":", attr(terms(formula(out$mods$final.mod[[1]])),"term.labels")))
-      if(Model=="rf")               nvar <- nrow(out$mods$summary)           
+               
             pcol <- min(ceiling(sqrt(nvar)),4)
             prow <- min(ceiling(nvar/pcol),3)
-
-            pdf(paste(bname,"_response_curves.pdf",sep=""),width=11,height=8.5,onefile=T)
-                par(oma=c(2,2,4,2),mfrow=c(prow,pcol))
+      
                      
         #this little section is borrowed from BIOMOD because rf partial plot 
         #does something odd with the y axis
@@ -71,33 +69,38 @@ response.curves<-function(out,Model,pred.dat=NULL,cv=FALSE){
                 levels(Xp[, i]) <- levels(dat[, i])
             }
     }
-  
-     for (i in sort(match(out$mods$vnames,names(dat)))) {
-            if (!is.factor(dat[, i])) {
-                xr <- range(dat[, i])
-                Xp1 <- Xp
-                Xp1[, i] <- seq(xr[1], xr[2], len = nrow(dat))
-            }
-            else {
-                Xp1 <- Xp
-                Nrepcat <- floor(nrow(dat)/length(levels(dat[,
-                  i])))
-                Xp1[, i] <- as.factor(c(rep(levels(dat[, i])[1],
-                  nrow(dat) - (Nrepcat * length(levels(dat[,
-                    i])))), rep(levels(dat[, i]), each = Nrepcat)))
-            }
-            Xf<-matrix(nrow=nrow(Xp1),ncol=length(out$mods$final.mod))
-            for(j in 1:length(out$mods$final.mod)){
-                  Xf[,j] <- pred.fct(out$mods$final.mod[[j]], as.data.frame(Xp1),Model)
-             }
-                  y.lim<-c(0,1)
-                 y.lim=range(apply(Xf,1,mean))
-                   plot(Xp1[, i],apply(Xf,1,mean), ylim = y.lim, xlab = "",
-                  ylab = "", type = "l", main = names(dat)[i])       
-           } 
-            graphics.off()     
+     dir.create(paste(out$input$output.dir,"\\responseCurves",sep=""))
+      for (k in c(1,2)){
+          if(k==1){ jpeg(paste(out$input$output.dir,"\\responseCurves\\","all_response_curves.jpg",sep=""),width=2000,height=2000,quality=100)
+                    par(oma=c(2,2,4,2),mfrow=c(prow,pcol))}                   
+         for (i in sort(match(out$mods$vnames,names(dat)))) {
+                if (k==2) jpeg(filename=paste(out$input$output.dir,"\\responseCurves\\",names(dat)[i],".jpg",sep=""),width=2000,height=2000,quality=100)
+                if (!is.factor(dat[, i])) {
+                    xr <- range(dat[, i])
+                    Xp1 <- Xp
+                    Xp1[, i] <- seq(xr[1], xr[2], len = nrow(dat))
+                }
+                else {
+                    Xp1 <- Xp
+                    Nrepcat <- floor(nrow(dat)/length(levels(dat[,
+                      i])))
+                    Xp1[, i] <- as.factor(c(rep(levels(dat[, i])[1],
+                      nrow(dat) - (Nrepcat * length(levels(dat[,
+                        i])))), rep(levels(dat[, i]), each = Nrepcat)))
+                }
+                Xf<-matrix(nrow=nrow(Xp1),ncol=length(out$mods$final.mod))
+                for(j in 1:length(out$mods$final.mod)){
+                      Xf[,j] <- pred.fct(out$mods$final.mod[[j]], as.data.frame(Xp1),Model)
+                 }
+                      y.lim<-c(0,1)
+                     y.lim=range(apply(Xf,1,mean))
+                       plot(Xp1[, i],apply(Xf,1,mean), ylim = y.lim, xlab = "",
+                      ylab = "", type = "l", main = names(dat)[i],lwd=2,cex=3,cex.main=3,cex.axis=2.5)
+               if(k==2) graphics.off()              
+               } 
+         if(k==1) graphics.off()     
        }         
-                
+  }         
                 
                 
                 
