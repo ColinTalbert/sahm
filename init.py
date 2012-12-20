283672fe7eeb83859fcea99573c6bfa8b8a2243c
Colin Talbert <talbertc@usgs.gov>
2012-12-20 12:11:55 -0700

    test GIT functioning

--- init.py
+++ init.py
@@ -41,6 +41,7 @@
 ##
 ## Any use of trade, firm, or product names is for descriptive purposes only 
 ## and does not imply endorsement by the U.S. Government.
+##test
 ###############################################################################
 
 import csv
