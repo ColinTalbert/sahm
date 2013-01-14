d95ed153ab2ceb4b0a18311faa7c45474978597b
Marian <mtalbert@usgs.gov>
2013-01-14 09:38:13 -0700

    for new data

--- pySAHM/Resources/R_Modules/Testing/YetAnotherDebug.r
+++ pySAHM/Resources/R_Modules/Testing/YetAnotherDebug.r
@@ -1,107 +1,211 @@
-setwd("I:\\VisTrails\\VisTrails_SAHM_x32_debug\\VisTrails\\vistrails\\packages\\sahm_MarianDev\\pySAHM\\Resources\\R_Modules")
-ScriptPath="I:\\VisTrails\\VisTrails_SAHM_x32_debug\\VisTrails\\vistrails\\packages\\sahm_MarianDev\\pySAHM\\Resources\\R_Modules"
+#debug branch
+setwd("I:\\VisTrails\\VisTrails_SAHM_x64_debug\\VisTrails\\vistrails\\packages\\sahm_MarianDev\\pySAHM\\Resources\\R_Modules")
+ScriptPath="I:\\VisTrails\\VisTrails_SAHM_x64_debug\\VisTrails\\vistrails\\packages\\sahm_MarianDev\\pySAHM\\Resources\\R_Modules"
+dir.path<-"I:\\VisTrails\\WorkingFiles\\workspace\\_DebugTesting\\Debug1.10"
 
-#The idea here is to run this code then change above directories then rerun with the new code and compare (using append output and possible comparing maps)
-ScriptPath="I:\\VisTrails\\VisTrails_SAHM_x32_debug\\VisTrails\\vistrails\\packages\\sahm_MarianDev\\pySAHM\\Resources\\R_Modules"
-source("FIT_BRT_pluggable.r")
-ScriptPath="I:\\VisTrails\\VisTrails_SAHM_x32_debug\\VisTrails\\vistrails\\packages\\sahm_MarianDev\\pySAHM\\Resources\\R_Modules"
-source("FIT_MARS_pluggable.r")
-ScriptPath="I:\\VisTrails\\VisTrails_SAHM_x32_debug\\VisTrails\\vistrails\\packages\\sahm_MarianDev\\pySAHM\\Resources\\R_Modules"
-source("FIT_RF_pluggable.r")
-ScriptPath="I:\\VisTrails\\VisTrails_SAHM_x32_debug\\VisTrails\\vistrails\\packages\\sahm_MarianDev\\pySAHM\\Resources\\R_Modules"
-source("FIT_GLM_pluggable.r")
+#master branch
+#setwd("I:\\VisTrails\\VisTrails_SAHM_x32\\VisTrails\\vistrails\\packages\\sahm\\pySAHM\\Resources\\R_Modules")
+#ScriptPath="I:\\VisTrails\\VisTrails_SAHM_x32\\VisTrails\\vistrails\\packages\\sahm\\pySAHM\\Resources\\R_Modules"
+#dir.path<-"C:\\temp\\AcrossModelPerformanceDetailsForTesting\\MasterBranch7.12"
+#For Model tests
 source("LoadRequiredCode.r")
-source("RF.helper.fcts.r")
-source("BRT.helper.fcts.r")
-source("GLM.helper.fcts.r")
 source("MARS.helper.fcts.r")
-rc=c(rep("responseBinary",times=11),rep("responseCount",times=2))
+source("GLM.helper.fcts.r")
+source("BRT.helper.fcts.r")
+source("RF.helper.fcts.r")
+
+#For Apply Model Tests
+source("EvaluateNewData.r")
+
+#For PairsExplore and parameter inspection
+source("PairsExplore.r")
+source("Predictor.inspection.r")
+source("my.panel.smooth.binary.r")
+
+#For Data Splitting
+source("TestTrainSplit.r")
+source("CrossValidationSplit.r")
+
+
+
+rc=c(rep("responseBinary",times=10),rep("responseCount",times=2))
 input.file<-vector()
-input.file[1]="C:/VisTrails/mtalbert_20110504T132851/readMaTests/BadPath.csv"
-input.file[2]="I:\\VisTrails\\VisTrails_SAHM_x32_debug\\VisTrails\\vistrails\\packages\\TestingRCode\\CrossValidationSp1test.csv"
-input.file[3]="C:/VisTrails/mtalbert_20110504T132851/readMaTests/Split.csv"
-input.file[4]="C:/VisTrails/mtalbert_20110504T132851/readMaTests/SplitBadFactor.csv"
-input.file[5]="C:/VisTrails/mtalbert_20110504T132851/readMaTests/SplitFactor.csv"
-input.file[6]="C:/VisTrails/mtalbert_20110504T132851/readMaTests/Factor.csv"
-input.file[7]="C:/VisTrails/mtalbert_20110504T132851/readMaTests/SplitFactor2.csv"
-input.file[8]="C:/VisTrails/mtalbert_20110504T132851/readMaTests/SplitWeights.csv"
-input.file[9]="I:\\VisTrails\\VisTrails_SAHM_x32_debug\\VisTrails\\vistrails\\packages\\TestingRCode\\UsedAvailableSp1NoCV.csv"
-input.file[10]="C:/VisTrails/mtalbert_20110504T132851/readMaTests/NoSplit.csv"
-input.file[11]="C:/VisTrails/mtalbert_20110504T132851/readMaTests/SplitCrossVal.csv"
-input.file[12]="C:/VisTrails/mtalbert_20110504T132851/readMaTests/Count.csv"
-input.file[13]="C:/VisTrails/mtalbert_20110504T132851/readMaTests/CountSplit.csv"
+input.file=c(#used/available
+       "I:\\VisTrails\\WorkingFiles\\workspace\\_condor\\rf_1\\modelSelection_cv_1.csv",
+      "I:\\VisTrails\\WorkingFiles\\workspace\\_64xTesting\\CovariateCorrelationOutputMDS.csv",
+      ## Nonspatial data should work through SAHM
+      "I:\\VisTrails\\WorkingFiles\\workspace\\_DebugTesting\\mtalbert_20110504T132851\\readMaTests\\NonSpatialData.csv",      
+      
+        #pres/abs
+      "I:\\VisTrails\\WorkingFiles\\workspace\\_DebugTesting\\mtalbert_20110504T132851\\readMaTests\\BadPath.csv",
+      "I:\\VisTrails\\WorkingFiles\\workspace\\_DebugTesting\\mtalbert_20110504T132851\\readMaTests\\Split.csv",
+      "I:\\VisTrails\\WorkingFiles\\workspace\\_DebugTesting\\mtalbert_20110504T132851\\readMaTests\\SplitCrossVal.csv",
+      "I:\\VisTrails\\WorkingFiles\\workspace\\_DebugTesting\\mtalbert_20110504T132851\\readMaTests\\SplitFactor.csv",
+      "I:\\VisTrails\\WorkingFiles\\workspace\\_DebugTesting\\mtalbert_20110504T132851\\readMaTests\\Factor.csv",
+      "I:\\VisTrails\\WorkingFiles\\workspace\\_DebugTesting\\mtalbert_20110504T132851\\readMaTests\\SplitWeights.csv",  
+      "I:\\VisTrails\\WorkingFiles\\workspace\\_DebugTesting\\mtalbert_20110504T132851\\readMaTests\\NoSplit.csv", 
+       #count
+      "I:\\VisTrails\\WorkingFiles\\workspace\\_DebugTesting\\mtalbert_20110504T132851\\readMaTests\\Count.csv",
+      "I:\\VisTrails\\WorkingFiles\\workspace\\_DebugTesting\\mtalbert_20110504T132851\\readMaTests\\CountSplit.csv")
 
 #I'm cutting these out of the standard test suite because they take a long time to run
 #and only test whether we run well on large datasets or big tiffs
 #"C:/VisTrails/mtalbert_20110504T132851/readMaTests/CanadaThistleNewFormat.csv"
 #"C:/VisTrails/mtalbert_20110504T132851/readMaTests/LargeSplit.csv"
-
+#add a missing data csv and maybe a couple with pseudo absence
 output.dir<-vector()
-output.dir[1]<-"C:\\temp\\AcrossModelPerformanceDetailsForTesting\\NewMasterBranch2.10\\rf"
-output.dir[2]<-"C:\\temp\\AcrossModelPerformanceDetailsForTesting\\NewMasterBranch2.10\\brt"
-output.dir[3]<-"C:\\temp\\AcrossModelPerformanceDetailsForTesting\\NewMasterBranch2.10\\mars"
-output.dir[4]<-"C:\\temp\\AcrossModelPerformanceDetailsForTesting\\NewMasterBranch2.10\\glm"
+output.dir[1]<-paste(dir.path,"\\rf",sep="")
+output.dir[2]<-paste(dir.path,"\\brt",sep="")
+output.dir[3]<-paste(dir.path,"\\mars",sep="")
+output.dir[4]<-paste(dir.path,"\\glm",sep="")
+output.dir[5]<-paste(dir.path,"\\maxlike",sep="")
 
-##BRT
- for(i in 1:length(input.file)){
-try(FitModels(ma.name=input.file[i],
-          tif.dir=NULL,output.dir=output.dir[2],
-          response.col=rc[i],make.p.tif=T,make.binary.tif=T,n.folds=3,simp.method="cross-validation",tc=NULL,alpha=1,
-      family = "bernoulli",max.trees = 10000,tolerance.method = "auto",
-  tolerance = 0.001,seed=1,opt.methods=2,
-          simp.method="cross-validation",debug.mode=T,responseCurveForm="pdf",script.name="brt",
-          learning.rate =NULL, bag.fraction = 0.5,prev.stratify = TRUE, max.trees = NULL,opt.methods=2,save.model=TRUE,MESS=TRUE))
-        }
-   # PredictModel(workspace=paste(output.dir,"modelWorkspace",sep="\\"),out.dir="C:\\VisTrails")
 
-##MARS
+########   Model Fit Test  ###########
+        ##BRT
+         for(i in 1:length(input.file)){
+              try(FitModels(ma.name=input.file[i],
+                        tif.dir=NULL,output.dir=output.dir[2],
+                        response.col=rc[i],make.p.tif=T,make.binary.tif=F,n.folds=3,simp.method="cross-validation",tc=NULL,alpha=1,
+                    family = "bernoulli",max.trees = 10000,tolerance.method = "auto",
+                tolerance = 0.001,seed=1,opt.methods=2,
+                        simp.method="cross-validation",debug.mode=T,responseCurveForm="pdf",script.name="brt",
+                        learning.rate =NULL, bag.fraction = 0.5,prev.stratify = TRUE, max.trees = NULL,opt.methods=2,save.model=TRUE,MESS=T,ScriptPath=ScriptPath))
+                      }
+              
+              
+              ##MARS
+              for(i in 1:length(input.file)){
+                  try(FitModels(ma.name=input.file[i],
+                          tif.dir=NULL,output.dir=output.dir[3],
+                          response.col=rc[i],make.p.tif=T,make.binary.tif=T,
+                          mars.degree=1,mars.penalty=2,debug.mode=T,responseCurveForm="pdf",script.name="mars",opt.methods=2,MESS=TRUE,ScriptPath=ScriptPath))
+                      }
+                
+              
+              ##GLM
+              for(i in 1:length(input.file)){
+                  try(FitModels(ma.name=input.file[i],
+                        tif.dir=NULL,
+                        output.dir=output.dir[4],
+                        response.col=rc[i],make.p.tif=T,make.binary.tif=F,
+                        simp.method="AIC",debug.mode=T,responseCurveForm="pdf",script.name="glm",MESS=FALSE,opt.methods=2,squared.terms=TRUE,ScriptPath=ScriptPath))
+                        }
+              
+              ### Random Forest
+              for(i in 1:length(input.file)){
+              proximity=NULL
+              try(FitModels(ma.name=input.file[i],
+                    tif.dir=NULL,
+                    output.dir=output.dir[1],
+                    response.col=rc[i],make.p.tif=T,make.binary.tif=F,
+                        debug.mode=T,opt.methods=2,script.name="rf",
+              responseCurveForm="pdf",xtest=NULL,ytest=NULL,n.trees=1000,mtry=NULL,
+              samp.replace=FALSE,sampsize=NULL,nodesize=NULL,maxnodes=NULL,importance=FALSE,
+              localImp=FALSE,nPerm=1,proximity=NULL,oob.prox=proximity,norm.votes=TRUE,
+              do.trace=FALSE,keep.forest=NULL,keep.inbag=FALSE,save.model=TRUE,MESS=F,seed=1,ScriptPath=ScriptPath))
+                 }
+ 
+              ### Maxlike
+              Formula="~bio_06_2000_2km + bio_14_2000_4km + NDVI_annualMaximumValue_2009 + NDVI_greenuprates1_2003 + NDVI_peakdates1_2003"
+            
+               for(i in 1:2){
+                try(FitModels(ma.name=input.file[i],
+                		tif.dir=NULL,
+                		output.dir=output.dir[5],
+                		response.col=rc[i],
+                		make.p.tif=T,make.binary.tif=T,
+                		debug.mode=T,responseCurveForm="pdf",script.name="maxlike",
+                		opt.methods=2,MESS=T,Formula=Formula,UseTiffs=FALSE,ScriptPath=ScriptPath))
+              }
+              		
+### Pairs Explore Tests  #####
+source("PairsExplore.r")
+source("read.dat.r")
+source("chk.libs.r")
+source("read.dat.r")
+source("my.panel.smooth.binary.r")
+source("Predictor.inspection.r")
+dir.path<-"I:\\VisTrails\\WorkingFiles\\workspace\\_DebugTesting\\Debug1.10\\PredInspect""
+input.file<-vector()
+
+input.file[1]="I:\\VisTrails\\WorkingFiles\\workspace\\_DebugTesting\\mtalbert_20110504T132851\\readMaTests\\Split.csv"
+input.file[2]="I:\\VisTrails\\WorkingFiles\\workspace\\_DebugTesting\\mtalbert_20110504T132851\\readMaTests\\SplitFactor.csv"
+input.file[3]="I:\\VisTrails\\WorkingFiles\\workspace\\_DebugTesting\\mtalbert_20110504T132851\\readMaTests\\SplitWeights.csv"
+input.file[4]="I:\\VisTrails\\WorkingFiles\\workspace\\_DebugTesting\\mtalbert_20110504T132851\\readMaTests\\CanadaThistleMissingDat.csv"
+input.file[5]="I:\\VisTrails\\WorkingFiles\\workspace\\_DebugTesting\\mtalbert_20110504T132851\\readMaTests\\SplitCrossVal.csv"
+input.file[6]="I:\\VisTrails\\WorkingFiles\\workspace\\_DebugTesting\\mtalbert_20110504T132851\\readMaTests\\Count.csv"
+##pseudoabs
+input.file[7]="I:\\VisTrails\\VisTrails_SAHM_x32_debug\\VisTrails\\vistrails\\packages\\TestingRCode\\ElithPsdoAbs.csv"
+      ## Nonspatial data should work through SAHM
+input.file[8]="I:\\VisTrails\\WorkingFiles\\workspace\\_DebugTesting\\mtalbert_20110504T132851\\readMaTests\\NonSpatialData.csv"   
+predictor<-c("bio_13_wgs84","bio_15_wgs84_categorical","bio_7_wgs84","asp_2k_alb","bio_16_wgs84","dem","Temperature")
+responseCol<-c(rep("responseBinary",times=6),rep("responseCount",times=1))
+
 for(i in 1:length(input.file)){
-    try(FitModels(ma.name=input.file[i],
-            tif.dir=NULL,output.dir=output.dir[3],
-            response.col=rc[i],make.p.tif=T,make.binary.tif=T,
-            mars.degree=1,mars.penalty=2,debug.mode=T,responseCurveForm="pdf",script.name="mars",opt.methods=2,MESS=TRUE))
-        }
-   # PredictModel(workspace=paste(output.dir,"modelWorkspace",sep="\\"),out.dir="C:\\VisTrails")
-
-##GLM
-for(i in 1:length(input.file)){
-    try(FitModels(ma.name=input.file[i],
-          tif.dir=NULL,
-          output.dir=output.dir[4],
-          response.col=rc[i],make.p.tif=T,make.binary.tif=T,
-          simp.method="AIC",debug.mode=T,responseCurveForm="pdf",script.name="glm",MESS=TRUE,opt.methods=2))
-          }
-
-### Random Forest
-for(i in 1:length(input.file)){
-proximity=NULL
-try(FitModels(ma.name=input.file[i],
-      tif.dir=NULL,
-      output.dir=output.dir[1],
-      response.col=rc[i],make.p.tif=T,make.binary.tif=T,
-          debug.mode=T,opt.methods=2,script.name="rf",
-responseCurveForm="pdf",xtest=NULL,ytest=NULL,n.trees=1000,mtry=NULL,
-samp.replace=FALSE,sampsize=NULL,nodesize=NULL,maxnodes=NULL,importance=FALSE,
-localImp=FALSE,nPerm=1,proximity=NULL,oob.prox=proximity,norm.votes=TRUE,
-do.trace=FALSE,keep.forest=NULL,keep.inbag=FALSE,save.model=TRUE,MESS=TRUE,seed=1))
-         }
-
-#Predictor Inspection Tests
-predictor="romoveg_rc_categorical"
-infil<-vector()
-infil[1]="C:/VisTrails/mtalbert_20110504T132851/readMaTests/UsedAvailable.csv"
-infil[2]="C:\\VisTrails\\mtalbert_20110504T132851\\readMaTests\\Split.csv"
-
-predictor<-vector()
-predictor[1]<-"romoveg_rc_categorical"
-predictor[2]="bio_13_wgs84"
-
-for(i in 1:length(input.file)){
-      try(Predictor.inspection(predictor[i],
-          input.file=infil[i],
-      		output.dir=output.dir,
-      		response.col=response.col,
-      		pres=TRUE,
-      		absn=TRUE,
-      		bgd=TRUE))
+   if(i==1) { 
+       try(Pairs.Explore(num.plots=5,
+                min.cor=.5,
+                input.file=input.file[i],
+            		output.file=paste(dir.path,"\\PairsExploreTest2\\",i,"Par1",".jpg",sep=""),
+            		response.col=responseCol[i],
+            		pres=TRUE,
+            		absn=TRUE,
+            		bgd=TRUE))
+        try(Pairs.Explore(num.plots=10,
+                min.cor=.5,
+                input.file=input.file[i],
+            		output.file=paste(dir.path,"\\PairsExploreTest2\\",i,"Par2",".jpg",sep=""),
+            		response.col=responseCol[i],
+            		pres=TRUE,
+            		absn=FALSE,
+            		bgd=FALSE,
+                cors.w.highest=TRUE))
+       try(Predictor.inspection(predictor[i],
+                input.file=input.file[i],
+            		output.dir=paste(dir.path,"\\PairsExploreTest2",sep=""),
+            		response.col=responseCol[i],
+            		pres=TRUE,
+            		absn=TRUE,
+            		bgd=TRUE))              				
+    }		
+ try(Pairs.Explore(num.plots=15,
+    min.cor=min.cor,
+    input.file=input.file[i],
+		output.file=paste(dir.path,"\\PairsExploreTest2\\",i,".jpg",sep=""),
+		response.col=responseCol[i],
+		pres=TRUE,
+		absn=TRUE,
+		bgd=TRUE))
+		
+	try(Predictor.inspection(predictor[i],
+    input.file[i],
+		output.dir=paste(dir.path,"\\PairsExploreTest2",sep=""),
+		response.col=responseCol[i],
+		pres=TRUE,
+		absn=TRUE,
+		bgd=TRUE))
 		}
+
+input.file<-"I:\\VisTrails\\WorkingFiles\\workspace\\_DebugTesting\\mtalbert_20110504T132851\\readMaTests\\CanadaThistleNewFormat.csv"
+input.file<-"I:\\VisTrails\\WorkingFiles\\workspace\\_DebugTesting\\mtalbert_20110504T132851\\readMaTests\\CanadaThistleMissingDat.csv"
+for (i in 5:25){ 
+ try(Pairs.Explore(num.plots=i,
+                min.cor=.5,
+                input.file=input.file,
+            		output.file=paste(dir.path,"\\PairsExploreTest2\\",i,"NumPlotsTest",".jpg",sep=""),
+            		response.col=responseCol[1],
+            		pres=TRUE,
+            		absn=TRUE,
+            		bgd=TRUE
+                ))
+            		}
+### Apply Model Test
+
+input.workspace=list(
+
+for(i in 1:length(input.workspace){
+EvaluateNewData(workspace=paste(output.dir,"modelWorkspace",sep="\\"),out.dir=output.dir,b.tif=TRUE,p.tif=TRUE,mess=TRUE,
+new.tifs="I:\\VisTrails\\WorkingFiles\\workspace\\_applyModel\\Error\\MergedDataset_10.csv",produce.metrics=TRUE,ScriptPath=ScriptPath)
+}
+### Data Splitting Tests
