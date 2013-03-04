4dc332880131b425cca4fb11a31b38d6e31d067e
Colin Talbert <talbertc@usgs.gov>
2013-03-04 15:27:31 -0700

    Fixed bug introduced by Merge of master into parallelR

--- init.py
+++ init.py
@@ -1759,7 +1759,7 @@ class MAXENT(Model):
         ourMaxent = MaxentRunner.MAXENTRunner()
         ourMaxent.outputDir = utils.mknextdir(prefix='maxent_')
         
-        ourMaxent.inputMDS = utils.getFileRelativeToCurrentVT(self.forceGetInputFromPort('inputMDS').name)
+        ourMaxent.inputMDS = utils.getFileRelativeToCurrentVT(self.forceGetInputFromPort('mdsFile').name)
         
         ourMaxent.maxentpath = maxent_path
         
