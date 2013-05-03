3a84d5b6562edc675cd3b6389e8d2986bd6c0adb
Marian <mtalbert@usgs.gov>
2013-05-03 15:01:15 -0600

    Colin's apply model bug

--- pySAHM/Resources/R_Modules/Testing/QuickDebug.r
+++ pySAHM/Resources/R_Modules/Testing/QuickDebug.r
@@ -7,8 +7,8 @@
 setwd("I:\\VisTrails\\DevWorkspace\\Marian\\userpackages\\sahm\\pySAHM\\Resources\\R_Modules")
 ScriptPath="I:\\VisTrails\\DevWorkspace\\Marian\\userpackages\\sahm\\pySAHM\\Resources\\R_Modules"
 
-setwd("I:\\VisTrails\\DevWorkspace\\Colin\\userpackages\\sahm\\pySAHM\\Resources\\R_Modules")
-ScriptPath="I:\\VisTrails\\DevWorkspace\\Colin\\userpackages\\sahm\\pySAHM\\Resources\\R_Modules"
+#setwd("I:\\VisTrails\\DevWorkspace\\Colin\\userpackages\\sahm\\pySAHM\\Resources\\R_Modules")
+#ScriptPath="I:\\VisTrails\\DevWorkspace\\Colin\\userpackages\\sahm\\pySAHM\\Resources\\R_Modules"
 source("LoadRequiredCode.r")
 source("MARS.helper.fcts.r")
 source("GLM.helper.fcts.r")
@@ -42,7 +42,7 @@ FitModels(ma.name=input.file,
 #================================================================#
 #                            MARS
 #================================================================#
-
+input.file="H:\\Desktop\\CheatgrassAnalysis\\BlockCV.csv"
 FitModels(ma.name=input.file,
             output.dir=output.dir,
             response.col=rc,make.p.tif=F,make.binary.tif=F,
@@ -52,16 +52,16 @@ total.time
 #================================================================#
 #                   Evaluate New Data
 #================================================================#
-ws<-"J:\\Projects\\Climate_RS_Comparison\\Cheatgrass_VisTrails\\WUS\\FEB2013\\mars_1\\modelWorkspace"
-output.dir="C:\\temp\\SAHMDebugJunk\\BRTOut1"
-new.tiffs="J://Projects//Climate_RS_Comparison//Cheatgrass_VisTrails//WUS//FEB2013//validation//MergedDataset_1.csv"
 
+new.tiffs="J:\\Projects\\NPScape\\DerivedData\\March2013\\MergedDataset_2.csv" 
+ws="J:\\Projects\\NPScape\\DerivedData\\March2013\\brt_1\\modelWorkspace" 
+output.dir="C:\\temp\\SAHMDebugJunk\\BRTOut1"
 load(ws)
 setwd("I:\\VisTrails\\DevWorkspace\\Marian\\userpackages\\sahm\\pySAHM\\Resources\\R_Modules")
 ScriptPath="I:\\VisTrails\\DevWorkspace\\Marian\\userpackages\\sahm\\pySAHM\\Resources\\R_Modules"
 source(file.path(ScriptPath,"LoadRequiredCode.r"))
 source(paste(toupper(out$input$script.name),".helper.fcts.r",sep=""))
-EvaluateNewData(workspace=ws,out.dir=output.dir,b.tif=FALSE,p.tif=FALSE,mess=FALSE,new.tifs=new.tiffs,out=out)
+EvaluateNewData(workspace=ws,out.dir=output.dir,b.tif=TRUE,p.tif=TRUE,mess=FALSE,new.tifs=new.tiffs,out=out)
 
 #================================================================#
 I:\VisTrails\VisTrails_SAHM_x64_debug\Central_R\R-2.14.1\bin\x64\Rterm.exe --vanilla -f I:\VisTrails\VisTrails_SAHM_x64_debug\VisTrails\vistrails\packages\sahm_MarianDev\pySAHM\Resources\R_Modules\EvaluateNewData.r --args mbt=TRUE mpt=TRUE ws="C:\temp\SAHM_workspace\rf_1\modelWorkspace" mes=FALSE o=C:\temp\SAHM_workspace\rf_3\modelWorkspace\rf_1
@@ -73,8 +73,8 @@ I:\VisTrails\VisTrails_SAHM_x64_debug\Central_R\R-2.14.1\bin\x64\Rterm.exe --van
 FitModels(ma.name=input.file,
           tif.dir=NULL,
           output.dir=output.dir,
-          response.col=rc,make.p.tif=T,make.binary.tif=T,
-          simp.method="AIC",debug.mode=T,responseCurveForm="pdf",script.name="glm",MESS=T,opt.methods=2,squared.terms=FALSE,ScriptPath=ScriptPath)
+          response.col=rc,make.p.tif=F,make.binary.tif=F,
+          simp.method="AIC",debug.mode=T,responseCurveForm="pdf",script.name="glm",MESS=F,opt.methods=2,squared.terms=FALSE,ScriptPath=ScriptPath)
 
    
 #================================================================#
