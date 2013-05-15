0cff9cbb271b0f7d3e4c0f24b92ef4e8c5b15a1a
Colin Talbert <talbertc@usgs.gov>
2013-05-15 08:41:42 -0600

    Changed multicore flag from mc to multicore in R models

--- pySAHM/Resources/R_Modules/FIT_MARS_pluggable.r
+++ pySAHM/Resources/R_Modules/FIT_MARS_pluggable.r
@@ -74,7 +74,7 @@ Args <- commandArgs(trailingOnly=FALSE)
     	if(argSplit[[1]][1]=="pen") mars.penalty <- as.numeric(argSplit[[1]][2])
     	if(argSplit[[1]][1]=="om")  opt.methods <- as.numeric(argSplit[[1]][2])
     	if(argSplit[[1]][1]=="mes")  MESS <- as.logical(argSplit[[1]][2])
-    	 if(argSplit[[1]][1]=="mc")  multCore <- as.logical(argSplit[[1]][2])
+    	 if(argSplit[[1]][1]=="multicore")  multCore <- as.logical(argSplit[[1]][2])
     }
 
 ScriptPath<-dirname(ScriptPath)
