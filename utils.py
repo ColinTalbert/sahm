c1b9910cd6d58ec1e26fe3a28a86ac9bdd8edfbd
Colin Talbert <talbertc@usgs.gov>
2013-09-03 11:58:28 -0600

    Cleaned up the hack I'm using to get the NoData, min, and max values from a raster.  The values stored in the file header are untrustworthy.

--- utils.py
+++ utils.py
@@ -100,11 +100,16 @@ def getpixelsize(filename):
 def getrasterminmax(filename):
     dataset = gdal.Open(filename, gdalconst.GA_ReadOnly)
     band = dataset.GetRasterBand(1)
+    nodata = getNDVal(filename)
     
-    min = band.GetMinimum()
-    max = band.GetMaximum()
-    if min is None or max is None or min == band.GetNoDataValue():
-            (min,max) = band.ComputeRasterMinMax(1)
+    _min = band.GetMinimum()
+    _max = band.GetMaximum()
+    
+    if _min is None or _max is None or _min == band.GetNoDataValue() or \
+    _min == nodata:
+        band.SetNoDataValue(float(nodata))
+        band.ComputeStatistics(True)
+        _min, _max = band.ComputeRasterMinMax(1)
 
     
 
@@ -118,10 +123,10 @@ def getrasterminmax(filename):
 #            band.SetNoDataValue(min)
 #            (min,max) = band.ComputeRasterMinMax(0)
         
-    if min == band.GetNoDataValue():
-        min = 0
+    if _min == band.GetNoDataValue():
+        _min = 0
             
-    return (min, max)
+    return (_min, _max)
     dataset = None
     
 def getNDVal(filename):
@@ -129,36 +134,14 @@ def getNDVal(filename):
     band = dataset.GetRasterBand(1)
     
     NDValue = band.GetNoDataValue()
-    
+    band.ComputeStatistics(True)
     min_pixel = band.GetMinimum()
-    upperLeftPixVal = band.ReadAsArray(0, 0, 1, 1, 1, 1)[0][0]
-    if min_pixel is not None and approx_equal(NDValue, min_pixel) or \
-        approx_equal(NDValue, upperLeftPixVal):
-            NDValue = band.ReadAsArray(0, 0, 1, 1, 1, 1)[0][0]
     
+    if approx_equal(min_pixel, -3.399999999999999961272e+38):
+        NDValue = min_pixel
+        band.SetNoDataValue(float(min_pixel))
+        band.ComputeStatistics(True)  
     
-    
-#    if min is None or min == band.GetNoDataValue():
-#            min = band.ComputeRasterMinMax(0)[0]
-
-    
-#    try:
-#        #our output rasters have approx nodata values
-#        #which don't equal the specified nodata.
-#        #set the specified to equal what is actually used.
-##        if (abs(band.GetNoDataValue() - min) < 1e-9 and 
-##            band.GetNoDataValue() <> min) or \
-##            ( min == band.GetNoDataValue()):
-##            min, max = band.ComputeRasterMinMax(0)
-##            band.SetNoDataValue(min)
-##            (min,max) = band.ComputeRasterMinMax(0)
-#        
-#        
-#        if approx_equal(NDValue, min):
-#            NDValue = min
-#        
-#    except:
-#        pass
     
     
     dataset = None
