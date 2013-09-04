184f7b90c0c5c325c2e2d3dd6fe0407f3680a909
Marian <mtalbert@usgs.gov>
2013-09-04 14:55:54 -0600

    Bug fix for bug 184 with apply model break this requires a bit more testing

--- pySAHM/Resources/R_Modules/EvaluateNewData.r
+++ pySAHM/Resources/R_Modules/EvaluateNewData.r
@@ -13,6 +13,8 @@ EvaluateNewData<-function(workspace=NULL,out.dir=NULL,b.tif=TRUE,p.tif=TRUE,mess
 #3. (Evaluate independent data or Evaluation Split) It will evaluate either data withheld for evaluation (using EvalSplit in the original mds) 
 #or evaluate a model on a completely new region if an mds is available for the new region new.tifs should be the mds and produce.metrics should be set to true 
 
+#produce metrics is true if there is a new mds file with more than just a header
+
 #Written by Marian Talbert 5/2012
      t0 <- unclass(Sys.time()) 
    
@@ -22,13 +24,14 @@ EvaluateNewData<-function(workspace=NULL,out.dir=NULL,b.tif=TRUE,p.tif=TRUE,mess
        try(rm(out),silent=TRUE)
        out<-out1
        out$input$ScriptPath=ScriptPath #this might not have been in the original, it's a new requirement
+     
        on.exit(capture.output(cat("Model Failed\n\n"),file=paste(out$dat$bname,"_output.txt",sep=""),append=TRUE))  
        source(file.path(ScriptPath,paste(toupper(out$input$script.name),".helper.fcts.r",sep="")))
     # generate a filename for output #
                 out$input$output.dir<-out.dir
                 out$input$MESS<-mess
                 Model<-out$input$script.name
-              out$dat$bname<-paste(out$input$output.dir,paste("/",Model,sep=""),sep="")
+              out$dat$bname<-paste(out$input$output.dir,paste("/","ApplyModel",sep=""),sep="")
                if(!produce.metrics & !is.null(new.tifs)) out$input$NoResidMaps=TRUE
          
     #write a few details on the data to the output txt document
@@ -52,9 +55,8 @@ EvaluateNewData<-function(workspace=NULL,out.dir=NULL,b.tif=TRUE,p.tif=TRUE,mess
          ##################################################################################
          ###################### Producing Evalutaiton Metrics for the hold out data
                  #if evaluating on brand new data switch out the mds 
-                
+                      store.train<-out$dat$ma$train
                  if(!is.null(new.tifs)){out$input$ma.name<-new.tifs
-                    store.train<-out$dat$ma$train
                     newDat<-try(read.csv(new.tifs,skip=4,nrows=2),silent=TRUE)
                     if(class(newDat)=="try-error") produce.metrics=FALSE
                  }
@@ -79,55 +81,54 @@ EvaluateNewData<-function(workspace=NULL,out.dir=NULL,b.tif=TRUE,p.tif=TRUE,mess
                      path.check(paths)
                       out$dat$tif.ind<-paths
                       out$dat$input$ParcTemplate<-tif.info[[3]][1]
+                  }else{
+                  #if we're missing a new mds we will evaluate the evaluation split switch it out here
+                      if(!is.na(Eval.split<-match("evalsplit",tolower(unlist(hl))))){
+                                      if(!is.na(Split<-match("split",tolower(unlist(hl))))) {
+                                        hl[[1]][Split]<-"Unused"
+                                        include[Split]<-0
+                                      }
+                                      hl[[1]][Eval.split]<-"Split"
+                      
+                      }
                   }
-             if(produce.metrics){   
-                   if(is.null(new.tifs)){ 
-                                 
+                 
+                  #if produce metrics is true we're evaluationg on them if new.tifs is null we're evaluating on the evaluation split
+             if(produce.metrics | is.null(new.tifs)){  
+                                  
                    #Determine if we will be evaluating a holdout set if so rename it to the split portion so evaluation metrics will be caluclated and reported
                    #Switching the name Eval.Split to split so we evaluate on final holdout data
-                           if(!is.na(Eval.split<-match("evalsplit",tolower(unlist(hl))))){
-                                  if(!is.na(Split<-match("split",tolower(unlist(hl))))) {
-                                    hl[[1]][Split]<-"Unused"
-                                    include[Split]<-0
-                                  }
-                                  hl[[1]][Eval.split]<-"Split"
+                                    
                                    out <- read.ma(out,hl=hl,include=include,evalNew=TRUE)
                                     out$dat$split.type=out$dat$split.label="eval"
-                                    out$dat$bname<-paste(out$input$output.dir,paste("/",Model,sep=""),sep="")
-                                  
-                         }
-                   }
-                                
-                      
+                                    out$dat$bname<-paste(out$input$output.dir,paste("/","ApplyModel",sep=""),sep="")                      
+                         
                       #if we have completely new data then the whole dataset should be used for testing and the original displayed as training data
-                      if(!is.null(new.tifs)) {
-                      out <- read.ma(out,hl=hl,include=include,evalNew=TRUE)
                          #read.ma wasn't designed for what it's doing here and overwrites a few things it shouldn't 
                          #these are reset correctly here
-                          names(out$dat$ma)<-"test"
-                          out$dat$ma$train<-store.train
-                            out$dat$split.type=out$dat$split.label="eval"
-                            out$dat$bname<-paste(out$input$output.dir,paste("/",Model,sep=""),sep="")
-                      }
+                          if(produce.metrics) names(out$dat$ma)<-"test"
+                              
+                              out$dat$ma$train<-store.train
+                          
+                             
                         out <- place.save(out,Final.Model=TRUE)
                   # Making Predictions
                            pred.vals<-function(x,model,Model){
                           x$pred<-pred.fct(model,x$dat[,2:ncol(x$dat)],Model)
                           return(x)}
-                          
-                       
-                         assign("out",out,envir=.GlobalEnv)
-                          #getting the predictions for the test/train split
-                          out$dat$ma<-(lapply(X=out$dat$ma,FUN=pred.vals,model=out$mods$final.mod,Model=Model))
-                          #Just for the training set for Random Forest we have to take out of bag predictions rather than the regular predictions
-                          if(Model=="rf") out$dat$ma$train$pred<-tweak.p(as.vector(predict(out$mods$final.mod[[1]],type="vote")[,2]))
-                    
-                          #producing auc and residual plots model summary information and accross model evaluation metric
                      
+                     
+                          #getting the predictions for the new evaluation data 
+                          #train predictions should be stored correctly in the original object
+                          out$dat$ma$test<-pred.vals(x=out$dat$ma$test,model=out$mods$final.mod,Model=Model)
+                          
+                          #producing auc and residual plots model summary information and accross model evaluation metric   
                        out$dat$bnameExpanded=file.path(dirname(out$dat$bname),"ExpandedOutput")
+                       assign("out",out,envir=.GlobalEnv)
                        dir.create(out$dat$bnameExpanded)
                        predsForOut<-cbind(resp=out$dat$ma$test$resp,pred=out$dat$ma$test$pred)
-                       write.csv(predsForOut,file.path(out$dat$bnameExpanded,"predicted.csv")) 
+                       write.csv(predsForOut,file.path(out$dat$bnameExpanded,"predicted.csv"))
+                       
                       out$mods$auc.output<-make.auc.plot.jpg(out=out)
                       response.curves(out,Model)
              
