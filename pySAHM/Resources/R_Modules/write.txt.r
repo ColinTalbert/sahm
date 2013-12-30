19260c5eedef3632771d9061c36fecbce5d7bca2
Marian <mtalbert@usgs.gov>
2013-12-30 14:59:27 -0700

    formatting change

--- pySAHM/Resources/R_Modules/write.txt.r
+++ pySAHM/Resources/R_Modules/write.txt.r
@@ -5,14 +5,14 @@ write.txt<-function(out,t0){
     rf="Random Forest Modeling Results\n",
     brt="\nBoosted Regression Tree Modeling Results\n",
     maxent="\nMAXENT\n")
-
+              browser()
   txt0 <- paste(model.label,"\n",
                       "Data:\n",out$input$ma.name,"\n",
-                           "\n\t n(pres) =                       ",out$dat$nPresAbs$train[2],
-              if(!PsdoAbs) "\n\t n(abs)  =                       ",
-              if(PsdoAbs)  "\n\t n(bkgd) =                       ",
+                           "\n\t n(pres)                 =      ",out$dat$nPresAbs$train[2],
+              if(!PsdoAbs) "\n\t n(abs)                  =      ",
+              if(PsdoAbs)  "\n\t n(bkgd)                 =      ",
                  out$dat$nPresAbs$train[1],
-              "\n\t n covariates considered =      ",length(out$dat$used.covs),"\n",
+                           "\n\t n covariates considered =      ",length(out$dat$used.covs),"\n",
               if(out$input$script.name!="maxent")"\n\t total time for model fitting = ",
                 round((unclass(Sys.time())-t0)/60,2),"min\n",sep="")
               
