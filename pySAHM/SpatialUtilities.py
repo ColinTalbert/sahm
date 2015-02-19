Fixed a missing arg so that averaged gtifs can use compression
Conflicts:

	pySAHM/SpatialUtilities.py

pySAHM/SpatialUtilities.py
@@ -683,7 +683,7 @@ def average_nparrays(arrays):
     return np.ma.mean(dstack, axis=2)
 
 def average_geotifs(raster_fnames, outfname,
-                    create_args=None, verbose=False):
+                    create_args=[], verbose=False):
     '''takes a list of raster fnames and saves
     the average of their pixel values to a new raster
     with the outfname
@@ -703,6 +703,8 @@ def average_geotifs(raster_fnames, outfname,
     out_raster = SAHMRaster(outfname)
     out_raster.pullParamsFromRaster(raster_fnames[0])
     out_raster.createNewRaster(create_args)
+    out_raster.createNewRaster()  #  create_args)
+    out_raster.createNewRaster(create_args)
 
     #  loop though the blocks in our output raster
     #  calculate the cooresponding block from the inputs
@@ -716,6 +718,8 @@ def average_geotifs(raster_fnames, outfname,
     for block in itertools.izip(*[sr.iterBlocks() for sr in rasters]):
         d = average_nparrays(block[:])
         out_raster.putBlock(d, sr.curCol, sr.curRow)
+        out_raster.putBlock(d,
+                            sr.curCol, sr.curRow)
 
         if verbose:
             print '\r>> Finished ' + str(cur_block) + ' out of ' + str(num_blocks),