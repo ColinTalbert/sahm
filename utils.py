6659b7a0d5add518b7fb96b320e80c9cb24974f3
Colin Talbert <talbertc@usgs.gov>
2015-06-18 07:39:44 -0600

    Merge conflict fix

--- utils.py
+++ utils.py
@@ -81,6 +81,7 @@ except:
 import numpy
 
 import pySAHM.utilities as utilities
+from CreatePredictorCurves import CreatePredictorCurvesDialog
 
 write_hash_entry_pickle = utilities.write_hash_entry_pickle
 delete_hash_entry_pickle = utilities.delete_hash_entry_pickle
@@ -594,6 +595,22 @@ def run_model_script(script, args_dict, module=None, runner_script="runRModel.py
     if not job_monitor.finished():
         writetolog("\nStarting processing of " + script , True)
         writetolog("    command used: \n" + utilities.convert_list_to_cmd_str(cmd), False, False)
+
+        if module.abbrev == "hsc":
+            orig_mds = args_dict['c'].replace(".csv", "_orig.csv")
+            shutil.copyfile(args_dict['c'], orig_mds)
+
+            json_fname = os.path.join(module.output_dname, 'hsc.json')
+            kwargs_mod = {'inputMDS':args_dict['c'],
+                      'output_json':json_fname}
+            args_dict['hsc'] = json_fname
+            cmd.append("hsc=" + json_fname)
+            dialog = CreatePredictorCurvesDialog(kwargs_mod)
+            #  dialog.setWindowFlags(QtCore.Qt.WindowMaximizeButtonHint)
+            retVal = dialog.exec_()
+            #  outputPredictorList = dialog.outputList
+            if retVal == 1:
+                raise ModuleError(module, "Cancel or Close selected (not OK) workflow halted.")
 
         if processing_mode == "FORT Condor":
             runModelOnCondor(script, args_dict)
