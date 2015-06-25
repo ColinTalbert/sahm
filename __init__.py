218def03f57747ea7e50b72a446d88f1a99cd743
Colin Talbert <talbertc@usgs.gov>
2015-06-25 14:29:05 -0600

    Upgrade version number to 2.0

--- __init__.py
+++ __init__.py
@@ -49,7 +49,7 @@ from vistrails.core.configuration import ConfigurationObject
 
 name = "SAHM"
 identifier = "gov.usgs.sahm"
-version = '1.2.0'
+version = '2.0.0'
 
 def package_dependencies():
     import vistrails.core.packagemanager
